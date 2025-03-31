d7bdb50c855eaf173a2e740188c8e2a3befa1d51
varyansan <varyan.san.98@gmail.com>
2025-03-31 15:21:34 +0300

    start

---
+++ src/main/java/com/example/shcool_hogwarts/SchoolHogwartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shcool_hogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolHogwartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shcool_hogwarts/SchoolHogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shcool_hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolHogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
